73bd3fc6efc22800f3d3d26ceb9395bfd39d9d44
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2023-10-11 12:32:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ class Animal {
   }
 
   makeSound() {
-    console.log("The animal makes a sound");
+    console.log(`The ${this._species} makes a sound`);
   }
 	}
 
@@ -28,9 +28,9 @@ class Cat extends Animal {
 window.Animal = Animal;
 window.Dog = Dog;
 window.Cat = Cat;
-const cat1 = new Cat("miccymouse");
+const cat1 = new Cat("Siamese");
 cat1.makeSound();
 cat1.purr();
-const dog1 = new Dog("tommy");
+const dog1 = new Dog("Golden Retriever");
 dog1.makeSound();
 dog1.bark();
